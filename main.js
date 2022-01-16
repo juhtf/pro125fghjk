1e78a866adf6580ecfc9791e0446eb4f0d94c297
juhtf <86907413+juhtf@users.noreply.github.com>
2022-01-16 15:11:50 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized!!!")
+}
+
+function draw(){
+    document.getElementById("square_side").innerHTML="Width and Height of the text is-" + difference + "px";
+    background("green");
+    fill('#F90093');
+    stroke('#F90093');
+    textSize(difference);
+    text("aarav",40,100);
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    noseX=results[0].pose.nose.x
+    noseY=results[0].pose.nose.y
+    console.log("noseX" + noseX + "noseY" + noseY);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    console.log("leftWristX" + leftWristX + "rightWristX" + rightWristX + "difference" + difference);
+}
+}
